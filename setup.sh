7bd18a25c6a3c552066b1c888b10a538794ee452
muhammedkaya <muhammedkaya@192.168.1.34>
2021-09-02 23:37:38 +0300

    db port changed.

--- setup.sh
+++ setup.sh
@@ -14,5 +14,27 @@ kubectl apply -f wp-svc.yaml
 kubectl apply -f ingress.yaml
 
 
-ipaddr=$(kubectl get ingress --namespace=default | grep wp-ingress | awk '{print $4}') && \
+
+
+
+# Sadece k8s.example.com ile gelmesi için
+# * k8s.example.com a karşılık node un ip adresi /etc/hosts a eklendi
+# * Ingress ile k8s.example.com dan gelen requestler ClusterIP wp-svc nin 8888 ine yönlendirildi.
+# * Apache configi değiştirilip, sadece k8s.example.com ServerName i olan requestler kabul edildi.
+
+
+ipaddr=$(kubectl get nodes -o wide | grep $1 | awk '{print $6}')
 sudo sed -i '1 i\'$ipaddr'\tk8s.example.com' /etc/hosts
+
+
+kubectl exec -it pod/wp-deployment-d8676558-f9xxk -- /bin/bash
+apt-get update
+apt-get install apt-file
+apt-file update
+apt-get install vim     # now finally this will work !!!
+
+vim /etc/apache2/sites-enabled/000-default.conf
+ServerName = k8s.example.com
+
+
+
